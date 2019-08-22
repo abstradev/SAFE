59ce2c2be83e006469117c6c7b4d3e216b72d61f
Tony Duco <nosduco@gmail.com>
2019-08-22 18:55:14 -0400

    basic webview stuff

--- safe-switcher/index.js
+++ safe-switcher/index.js
@@ -132,7 +132,11 @@ class App extends EventEmitter {
     this.icon = args.icon;
     this.src = args.src;
     this.appElements = {};
-    this.webviewAttributes = {};
+    this.webviewAttributes = {
+      autosize: 'on',
+      useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) safe/0.0.1 Chrome/69.0.3497.128 Electron/4.2.8 Safari/537.36',
+      allowpopups: 'on'
+    };
     this.webviewAttributes.src = args.src;
     this.webviewAttributes.id = args.title;
     AppPrivate.initApp.bind(this)();
