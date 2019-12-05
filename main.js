2b3eb69186d2efb59032acabf7b0d4d5338159ea
Tony Duco <nosduco@gmail.com>
2019-12-04 20:36:04 -0500

    added custom menu and about window

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow, Tray, Menu, nativeImage, shell } = require('electron')
 const contextMenu = require('electron-context-menu');
+const openAboutWindow = require('about-window').default;
 const path = require('path')
 
 let mainWindow
@@ -19,7 +20,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    title: 'Safe',
+    title: 'SAFE',
     webPreferences: {
       nodeIntegration: true,
       webviewTag: true
@@ -59,6 +60,70 @@ app.on('activate', function () {
 app.on('ready', () => {
   const imgPath = path.join(__dirname, 'resources', 'icon.png');
   const img = nativeImage.createFromPath(imgPath);
+
+  // Menu Bar
+  const menuBar = Menu.buildFromTemplate([
+    {
+      label: 'App',
+      submenu: [
+        {
+          label: 'Minimize',
+          click: async () => {
+            mainWindow.minimize();
+          }
+        },
+        {
+          label: 'Quit',
+          click: async () => {
+            app.isQuitting = true;
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      role: 'editMenu'
+    },
+    {
+      label: 'Tools',
+      submenu: [
+        { 
+          role: 'reload' 
+        },
+        { 
+          role: 'forcereload' 
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About SAFE',
+          click: () => openAboutWindow({
+            icon_path: imgPath,
+            product_name: 'SAFE',
+            copyright: 'Abstra LLC',
+            description: 'Single App For Everything',
+            win_options: {
+              autoHideMenuBar: true
+            },
+            use_version_info: true
+          })
+        },
+        {
+          label: 'Abstra LLC',
+          click: async () => {
+            const { shell } = require('electron');
+            await shell.openExternal('https://abstraconsulting.com')
+          }
+        }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menuBar);
+
+  // Tray
   tray = new Tray(img);
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Open', click: function () {
